b5222ae9bc43302ddbf2555e2eaf1afc5abfd32d
andrehve <56153762+andrehve@users.noreply.github.com>
2020-03-17 13:23:47 +0100

    rydder

--- 2oblig/Readme.txt
+++
@@ -1,1 +0,0 @@
-Oblig 2 - Legemidler og resepter
--- 2oblig/Vanlig.java
+++
@@ -1,8 +0,0 @@
-class Vanlig extends Legemiddel{
-
-    protected Vanlig(String navn, double pris, double virkestoff){
-        super(navn, pris, virkestoff);
-
-    }
-    
-}
